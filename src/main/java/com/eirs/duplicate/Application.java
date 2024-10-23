969a0c72ea0de96f4f151d5c25a6cfdb9d0c984b
Arun Mehta <arun271333@gmail.com>
2024-10-23 08:38:56 +0530

    Encryptable Enable

--- src/main/java/com/eirs/duplicate/Application.java
+++ src/main/java/com/eirs/duplicate/Application.java
@@ -2,6 +2,7 @@ package com.eirs.duplicate;
 
 import com.eirs.duplicate.monitors.HashMapMonitorTask;
 import com.eirs.duplicate.orchestrator.RecordDuplicateProcessor;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +16,7 @@ import java.time.format.DateTimeFormatter;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableEncryptableProperties
 public class Application {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
